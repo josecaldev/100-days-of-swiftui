adcd6b6cbd4a6612973010f5683ae259463a7b81
Juan José Calderón <dev@josecaldev.com>
2025-02-22 22:51:57 -0500

    Day 1 Completed

--- 00-BasicSwift/BasicSwift.playground/Pages/Types and Casting.xcplaygroundpage/Contents.swift
+++ 00-BasicSwift/BasicSwift.playground/Pages/Types and Casting.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 var juniorSalary = "low"
-let learning = "constant"
+let learning = "be constant"
 
 var description = """
 I am a Calisthenics \"expert\" 
@@ -9,16 +9,21 @@ as long as no one sees me
 """
 
 print(juniorSalary, learning)
-print(description.count)
+print(juniorSalary.uppercased())
+print(learning.count)
+print(learning.hasPrefix("be"))
+
+
 print(description)
 
 //= = = = = = = = = = = =  = = =
 
-let minimumSalary = 1000
+
 let seniorSalary = 100_000_000
 //The underscore used only for human reading ease
 
-let cubedSalary = minimumSalary ^ 3
+let minimumSalary = 1000
+let cubedSalary = pow(Double(minimumSalary), 2) // ^ is XOR in Swift, no exponential
 print(cubedSalary)
 
 let number = 0.1 + 0.2
@@ -29,6 +34,9 @@ let a = 1
 var b = 3.0
 let c = Double(a) + b
 let d = a + Int(b)
+
+let e = 3.141592653
+print("e: ", Int(e))
 
 b -= 2.0
 
